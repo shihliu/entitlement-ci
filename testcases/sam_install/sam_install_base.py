7c7ade446bed13f4bc7756665dbc642657faa081
sgao <sgao@redhat.com>
2015-05-05 17:49:10 +0800

    change fuction name run, conflict with unit test

--- testcases/sam_install/sam_install_base.py
+++ testcases/sam_install/sam_install_base.py
@@ -6,8 +6,8 @@ class SAM_Install_Base(unittest.TestCase):
 
     commander = Command()
 
-    def run(self, cmd, cmddesc=None, timeout=None):
-        return self.commander.run(cmd, timeout, cmddesc)
+#     def run(self, cmd, cmddesc=None, timeout=None):
+#         return self.self.commander.run(cmd, timeout, cmddesc)
 
     def install_sam(self, compose):
         self.__stop_iptables()
@@ -20,22 +20,22 @@ class SAM_Install_Base(unittest.TestCase):
 
     def __stop_iptables(self):
         cmd = "service iptables stop"
-        self.run(cmd)
+        self.commander.run(cmd)
         cmd = "chkconfig iptables off"
-        self.run(cmd)
+        self.commander.run(cmd)
         cmd = "service iptables save"
-        self.run(cmd)
+        self.commander.run(cmd)
 
     def __set_selinux(self):
         cmd = "setenforce 0"
-        self.run(cmd)
+        self.commander.run(cmd)
         cmd = "sed -i -e 's/SELINUX=.*/SELINUX=%s/g' /etc/sysconfig/selinux" % ("permissive")
-        self.run(cmd)
+        self.commander.run(cmd)
 
     def __auto_subscribe(self):
 #         too slow for local install, add rhel repo instead
         cmd = "subscription-manager register --username=qa@redhat.com --password=uBLybd5JSmkRHebA --auto-attach"
-        self.run(cmd)
+        self.commander.run(cmd)
 #         cmd = ('cat <<EOF > /etc/yum.repos.d/myrhel.repo\n'
 #             '[rhel]\n'
 #             'name=rhel\n'
@@ -49,7 +49,7 @@ class SAM_Install_Base(unittest.TestCase):
 #             'gpgcheck=0\n'
 #             'EOF'
 #             )
-        self.run(cmd)
+        self.commander.run(cmd)
 
     def __add_sam_repo(self, sam_compose):
         cmd = ('cat <<EOF > /etc/yum.repos.d/sam.repo\n'
@@ -60,24 +60,24 @@ class SAM_Install_Base(unittest.TestCase):
             'gpgcheck=0\n'
             'EOF' % sam_compose
             )
-        self.run(cmd)
+        self.commander.run(cmd)
 
     def __install_katello(self):
         cmd = "yum install -y katello-headpin-all"
         # cmd = "yum install -y git"
-        self.run(cmd, timeout=7200)
+        self.commander.run(cmd, timeout=7200)
 
     def __deploy_sam(self):
         cmd = "katello-configure --deployment=sam --user-pass=admin"
-        self.run(cmd, timeout=1800)
+        self.commander.run(cmd, timeout=1800)
 
     def __import_manifest(self):
         # only support remote run
         self.__upload_manifest()
         cmd = "headpin -u admin -p admin provider import_manifest --org=ACME_Corporation --name='Red Hat' --file=/root/sam_install_manifest.zip"
-        self.run(cmd)
+        self.commander.run(cmd)
 
     def __upload_manifest(self):
         # self.remote_put(sam_manifest, "/root/%s" % manifest_name)
         cmd = "wget http://10.66.100.116/projects/sam-virtwho/latest-manifest/sam_install_manifest.zip -P /root/"
-        self.run(cmd)
+        self.commander.run(cmd)
