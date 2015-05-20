c28816dbe5224fa95819080adb386766ee564e15
sgao <sgao@redhat.com>
2015-05-20 18:18:00 +0800

    refact test case for esx

--- testcases/virt_who/esx/tc_ID155120_ESX_check_guest_consumer_uuid_on_server.py
+++ testcases/virt_who/esx/tc_ID155120_ESX_check_guest_consumer_uuid_on_server.py
@@ -1,53 +1,52 @@
-import commands, os, traceback, time
-from utils.Python import utils
-from repos.entitlement.ent_utils import ent_utils as eu
-from repos.entitlement.ent_env import ent_env as ee
+from utils import *
+from testcases.virt_who.virtwhobase import VIRTWHOBase
+from testcases.virt_who.virtwhoconstants import VIRTWHOConstants
+from utils.exception.failexception import FailException
 
-def tc_ID155120_ESX_check_guest_consumer_uuid_on_server(params):
-	''' Execute virt-who in a registered host to Check UUIDs of running guests '''
-	try:
-		try:
-			logger = params['logger']
-			eu().RESET_RESULT()
-			logger.info("=============== Begin of Running Test Case: %s ===============" % __name__[str.rindex(__name__, ".") + 1:])
-			guest_name = "ESX_" + params['handleguest']
-			destination_ip = ee.esx_host_ip
-			# Start a guest by start from host machine.
-			eu().esx_start_guest(logger, guest_name)
-			# Get guest IP
-			guestip = None
-			guestip = eu().esx_get_guest_ip(logger, guest_name, destination_ip)
-			if guestip == None:
-				logger.error("Faild to get guest ip.")
-				eu().SET_RESULT(1)
-			# Register guest to SAM
-			if not eu().sub_isregistered(logger, guestip):
-				eu().configure_host(logger, params.get("samhostname"), params.get("samhostip"), guestip)
-				eu().sub_register(logger, eu().get_env(logger)["username"], eu().get_env(logger)["password"], guestip)
-			# Get uuid of host and guest consumer
-			cmd = "grep 'Sending update in hosts-to-guests mapping' /var/log/rhsm/rhsm.log | tail -1"
-			ret, output = eu().runcmd(logger, cmd, "get host consumer uuid")
-			hostuuid = output.split("{")[1].split(":")[0].strip()
-			cmd = "subscription-manager identity | grep identity"
-			ret, output = eu().runcmd(logger, cmd, "get guest subscription-manager identity", guestip)
-			guestuuid = output.split(':')[1].strip()
-			# Check whether guest is included in host info
-			samhostip = params["samhostip"]
-			cmd = "curl -u admin:admin -k https://%s/sam/api/consumers/%s" % (samhostip, hostuuid)
-			ret, output = eu().runcmd(logger, cmd, "Check whether guest is included in host info")
-			if ret == 0 and guestuuid in output:
-				logger.info("Succeeded to check guest in host info.")
-				eu().SET_RESULT(0)
-			else:
-				logger.error("Failed to check guest in host info.")
-				eu().SET_RESULT(1)
-		except Exception, e:
-			logger.error("Test Failed due to error happened: " + str(e))
-			logger.error(traceback.format_exc())
-			eu().SET_RESULT(1)
-	finally:
-		if guestip != None:
-			eu().sub_unregister(logger, guestip)
-		eu().esx_stop_guest(logger, guest_name, destination_ip)
-		logger.info("=============== End of Running Test Case: %s ===============" % __name__[str.rindex(__name__, ".") + 1:])
-		return eu().TEST_RESULT()
+class tc_ID155120_ESX_check_guest_consumer_uuid_on_server(VIRTWHOBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            SAM_IP = get_exported_param("SAM_IP")
+            SAM_HOSTNAME = get_exported_param("SAM_HOSTNAME")
+            SAM_USER = VIRTWHOConstants().get_constant("SAM_USER")
+            SAM_PASS = VIRTWHOConstants().get_constant("SAM_PASS")
+
+            guest_name = VIRTWHOConstants().get_constant("ESX_GUEST_NAME")
+            destination_ip = VIRTWHOConstants().get_constant("ESX_HOST")
+
+            self.esx_start_guest(guest_name)
+            guestip = self.esx_get_guest_ip(guest_name, destination_ip)
+            guestuuid = self.esx_get_guest_uuid(guest_name, destination_ip)
+            # register guest to SAM
+            if not self.sub_isregistered(guestip):
+                self.configure_host(SAM_HOSTNAME, SAM_IP, guestip)
+                self.sub_register(SAM_USER, SAM_PASS, guestip)
+
+            # get uuid of host and guest consumer
+            cmd = "grep 'Sending update in hosts-to-guests mapping' /var/log/rhsm/rhsm.log | tail -1"
+            ret, output = self.runcmd(cmd, "get host consumer uuid")
+            hostuuid = output.split("{")[1].split(":")[0].strip()
+            cmd = "subscription-manager identity | grep identity"
+            ret, output = self.runcmd(logger, cmd, "get guest subscription-manager identity", guestip)
+            guestuuid = output.split(':')[1].strip()
+            # check whether guest is included in host info
+            cmd = "curl -u admin:admin -k https://%s/sam/api/consumers/%s" % (SAM_IP, hostuuid)
+            ret, output = self.runcmd(cmd, "Check whether guest is included in host info")
+            if ret == 0 and guestuuid in output:
+                logger.info("Succeeded to check guest in host info.")
+                self.assert_(True, case_name)
+            else:
+                raise FailException("Failed to check guest in host info.")
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            if guestip != None and guestip != "":
+                self.sub_unregister(guestip)
+            self.esx_stop_guest(guest_name, destination_ip)
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
+
+if __name__ == "__main__":
+    unittest.main()
