5344acd75a2e0ef7fe63f59ab318e6b95fe5d6bd
sgao <sgao@redhat.com>
2015-05-20 18:59:55 +0800

    refact test case

--- testcases/virt_who/esx/tc_ID155149_ESX_validate_complaince_after_starting_resuming.py
+++ testcases/virt_who/esx/tc_ID155149_ESX_validate_complaince_after_starting_resuming.py
@@ -1,68 +1,65 @@
-import commands, os, traceback, time
-from utils.Python import utils
-from repos.entitlement.ent_utils import ent_utils as eu
-from repos.entitlement.ent_env import ent_env as ee
+from utils import *
+from testcases.virt_who.virtwhobase import VIRTWHOBase
+from testcases.virt_who.virtwhoconstants import VIRTWHOConstants
+from utils.exception.failexception import FailException
 
-def tc_ID155149_ESX_validate_complaince_after_starting_resuming(params):
-	"""Execute virt-who with backgroud mode in a registered host via CLI"""
-	try:
-		try:
-			logger = params['logger']
-			eu().RESET_RESULT()
-			logger.info("=============== Begin of Running Test Case: %s ===============" % __name__[str.rindex(__name__, ".") + 1:])
-			guest_name = "ESX_" + params['handleguest']
-			samhostip = params['samhostip']
-			destination_ip = ee.esx_host_ip
-			host_uuid = eu().esx_get_host_uuid(logger, destination_ip)
-			# Start a guest by start from host machine.
-			eu().esx_start_guest(logger, guest_name)
-			# Get guest IP
-			guestip = None
-			guestip = eu().esx_get_guest_ip(logger, guest_name, destination_ip)
-			if guestip == None:
-				logger.error("Faild to get guest ip.")
-				eu().SET_RESULT(1)
-			# Register guest to SAM
-			if not eu().sub_isregistered(logger, guestip):
-				eu().configure_host(logger, params.get("samhostname"), params.get("samhostip"), guestip)
-				eu().sub_register(logger, eu().get_env(logger)["username"], eu().get_env(logger)["password"], guestip)
-			
-			# subscribe esx host with limited bonus subscription
-			eu().esx_subscribe_host_in_samserv(logger, host_uuid, eu().get_poolid_by_SKU(logger, ee.productid_guest) , samhostip)
-			
-			# Subscribe the registered guest to the corresponding bonus pool
-			eu().sub_subscribe_to_bonus_pool(logger, ee.productid_guest, guestip)
-			
-			# List consumed subscriptions on guest
-			eu().sub_listconsumed(logger, ee.productname_guest, guestip)
-			
-			# Pause a guest by suspend from host machine.
-			eu().esx_pause_guest(logger, guest_name, destination_ip)
-			# Resume a guest by resume from host machine.
-			eu().esx_resume_guest(logger, guest_name, destination_ip)
-			# Refresh the guest
-			eu().sub_refresh(logger, guestip)
-			# List consumed subscriptions on guest
-			eu().sub_listconsumed(logger, ee.productname_guest, guestip)
-			
-			# Shutdown a guest by shutdown from host machine.
-			eu().esx_stop_guest(logger, guest_name, destination_ip)
-			# Start a guest by start from host machine.
-			eu().esx_start_guest(logger, guest_name)
-			# Refresh the guest
-			eu().sub_refresh(logger, guestip)
-			# List consumed subscriptions on guest
-			eu().sub_listconsumed(logger, ee.productname_guest, guestip)
-			eu().SET_RESULT(0)
-		except Exception, e:
-			logger.error("Test Failed due to error happened: " + str(e))
-			logger.error(traceback.format_exc())
-			eu().SET_RESULT(1)
-	finally:
-		if guestip != None:
-			eu().sub_unregister(logger, guestip)
-		# Unregister the ESX host 
-		eu().esx_unsubscribe_all_host_in_samserv(logger, host_uuid, samhostip)
-		eu().esx_stop_guest(logger, guest_name, destination_ip)
-		logger.info("=============== End of Running Test Case: %s ===============" % __name__[str.rindex(__name__, ".") + 1:])
-		return eu().TEST_RESULT()
+class tc_ID155149_ESX_validate_complaince_after_starting_resuming(VIRTWHOBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            SAM_IP = get_exported_param("SAM_IP")
+            SAM_HOSTNAME = get_exported_param("SAM_HOSTNAME")
+            SAM_USER = VIRTWHOConstants().get_constant("SAM_USER")
+            SAM_PASS = VIRTWHOConstants().get_constant("SAM_PASS")
+
+            guest_name = VIRTWHOConstants().get_constant("ESX_GUEST_NAME")
+            destination_ip = VIRTWHOConstants().get_constant("ESX_HOST")
+
+            test_sku = VIRTWHOConstants().get_constant("productid_guest")
+            bonus_quantity = VIRTWHOConstants().get_constant("guestlimit")
+            sku_name = VIRTWHOConstants().get_constant("productname_guest")
+
+            host_uuid = self.esx_get_host_uuid(destination_ip)
+            self.esx_start_guest(guest_name)
+            guestip = self.esx_get_guest_ip(guest_name, destination_ip)
+            # register guest to SAM
+            if not self.sub_isregistered(guestip):
+                self.configure_host(SAM_HOSTNAME, SAM_IP, guestip)
+                self.sub_register(SAM_USER, SAM_PASS, guestip)
+            # subscribe esx host with limited bonus subscription
+            self.esx_subscribe_host_in_samserv(host_uuid, self.get_poolid_by_SKU(test_sku) , SAM_IP)
+            # subscribe the registered guest to the corresponding bonus pool
+            self.sub_subscribe_to_bonus_pool(test_sku, guestip)
+            # list consumed subscriptions on guest
+            self.sub_listconsumed(sku_name, guestip)
+            # pause a guest by suspend from host machine.
+            self.esx_pause_guest(guest_name, destination_ip)
+            # resume a guest by resume from host machine.
+            self.esx_resume_guest(guest_name, destination_ip)
+            # refresh the guest
+            self.sub_refresh(guestip)
+            # List consumed subscriptions on guest
+            self.sub_listconsumed(sku_name, guestip)
+            # shutdown a guest by shutdown from host machine.
+            self.esx_stop_guest(guest_name, destination_ip)
+            # start a guest by start from host machine.
+            self.esx_start_guest(guest_name)
+            # Refresh the guest
+            self.sub_refresh(guestip)
+            # List consumed subscriptions on guest
+            self.sub_listconsumed(sku_name, guestip)
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            if guestip != None and guestip != "":
+                self.sub_unregister(guestip)
+            self.esx_stop_guest(guest_name, destination_ip)
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
