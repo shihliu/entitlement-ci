785d7030fa5944e5a4ec76d841505ba7229faa47
sgao <sgao@redhat.com>
2015-05-26 16:27:43 +0800

    debug sam local install

--- testcases/sam_install/sam_install_base.py
+++ testcases/sam_install/sam_install_base.py
@@ -4,38 +4,41 @@ from utils.exception.failexception import FailException
 
 class SAM_Install_Base(unittest.TestCase):
 
-    commander = Command(get_exported_param("REMOTE_IP"), username=get_exported_param("REMOTE_USER"), password=get_exported_param("REMOTE_PASSWD"))
 
-#     def run(self, cmd, cmddesc=None, timeout=None):
-#         return self.self.commander.run(cmd, timeout, cmddesc)
+    def runcmd(self, cmd, cmddesc=None, targetmachine_ip=None, targetmachine_user=None, targetmachine_pass=None, timeout=None):
+        if targetmachine_ip != None and targetmachine_ip != "":
+            commander = Command(targetmachine_ip, targetmachine_user, targetmachine_pass)
+        else:
+            commander = Command(get_exported_param("REMOTE_IP"), username=get_exported_param("REMOTE_USER"), password=get_exported_param("REMOTE_PASSWD"))
+        return commander.run(cmd, timeout, cmddesc)
 
-    def install_sam(self, compose):
-        self.__stop_iptables()
-        self.__set_selinux()
-        self.__auto_subscribe()
-        self.__add_sam_repo(compose)
-        self.__install_katello()
-        self.__deploy_sam()
-        self.__import_manifest()
+    def install_sam(self, compose, server_ip=None, server_user=None, server_passwd=None):
+        self.__stop_iptables(server_ip, server_user, server_passwd)
+        self.__set_selinux(server_ip, server_user, server_passwd)
+        self.__auto_subscribe(server_ip, server_user, server_passwd)
+        self.__add_sam_repo(compose, server_ip, server_user, server_passwd)
+        self.__install_katello(server_ip, server_user, server_passwd)
+        self.__deploy_sam(server_ip, server_user, server_passwd)
+        self.__import_manifest(server_ip, server_user, server_passwd)
 
-    def __stop_iptables(self):
+    def __stop_iptables(self, server_ip=None, server_user=None, server_passwd=None):
         cmd = "service iptables stop"
-        self.commander.run(cmd)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd)
         cmd = "chkconfig iptables off"
-        self.commander.run(cmd)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd)
         cmd = "service iptables save"
-        self.commander.run(cmd)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd)
 
-    def __set_selinux(self):
+    def __set_selinux(self, server_ip=None, server_user=None, server_passwd=None):
         cmd = "setenforce 0"
-        self.commander.run(cmd)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd)
         cmd = "sed -i -e 's/SELINUX=.*/SELINUX=%s/g' /etc/sysconfig/selinux" % ("permissive")
-        self.commander.run(cmd)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd)
 
-    def __auto_subscribe(self):
+    def __auto_subscribe(self, server_ip=None, server_user=None, server_passwd=None):
 #         too slow for local install, add rhel repo instead
         cmd = "subscription-manager register --username=qa@redhat.com --password=uBLybd5JSmkRHebA --auto-attach"
-        self.commander.run(cmd)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd)
 #         cmd = ('cat <<EOF > /etc/yum.repos.d/myrhel.repo\n'
 #             '[rhel]\n'
 #             'name=rhel\n'
@@ -49,9 +52,9 @@ class SAM_Install_Base(unittest.TestCase):
 #             'gpgcheck=0\n'
 #             'EOF'
 #             )
-        self.commander.run(cmd)
+        self.runcmd(cmd)
 
-    def __add_sam_repo(self, sam_compose):
+    def __add_sam_repo(self, sam_compose, server_ip=None, server_user=None, server_passwd=None):
         cmd = ('cat <<EOF > /etc/yum.repos.d/sam.repo\n'
             '[sam]\n'
             'name=sam\n'
@@ -60,24 +63,24 @@ class SAM_Install_Base(unittest.TestCase):
             'gpgcheck=0\n'
             'EOF' % sam_compose
             )
-        self.commander.run(cmd)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd)
 
-    def __install_katello(self):
+    def __install_katello(self, server_ip=None, server_user=None, server_passwd=None):
         cmd = "yum install -y katello-headpin-all"
         # cmd = "yum install -y git"
-        self.commander.run(cmd, timeout=7200)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd, timeout=7200)
 
-    def __deploy_sam(self):
+    def __deploy_sam(self, server_ip=None, server_user=None, server_passwd=None):
         cmd = "katello-configure --deployment=sam --user-pass=admin"
-        self.commander.run(cmd, timeout=1800)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd, timeout=1800)
 
-    def __import_manifest(self):
+    def __import_manifest(self, server_ip=None, server_user=None, server_passwd=None):
         # only support remote run
         self.__upload_manifest()
         cmd = "headpin -u admin -p admin provider import_manifest --org=ACME_Corporation --name='Red Hat' --file=/root/sam_install_manifest.zip"
-        self.commander.run(cmd)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd)
 
-    def __upload_manifest(self):
+    def __upload_manifest(self, server_ip=None, server_user=None, server_passwd=None):
         # self.remote_put(sam_manifest, "/root/%s" % manifest_name)
         cmd = "wget http://10.66.100.116/projects/sam-virtwho/latest-manifest/sam_install_manifest.zip -P /root/"
-        self.commander.run(cmd)
+        self.runcmd(cmd, "", server_ip, server_user, server_passwd)
--- testcases/sam_install/sam_local_install.py
+++ testcases/sam_install/sam_local_install.py
@@ -1,3 +1,4 @@
+from utils import *
 from utils.tools.virshcommand import VirshCommand
 from testcases.sam_install.sam_install_base import SAM_Install_Base
 
@@ -6,11 +7,11 @@ class SAM_LOCAL_INSTALL(SAM_Install_Base):
     classdocs
     '''
     default_rhel_build = "http://download.englab.nay.redhat.com/pub/rhel/released/RHEL-6/6.6/Server/x86_64/os/"
-    default_sam_build = "http://download.devel.redhat.com/devel/candidate-trees/SAM/latest-SAM-1.4-RHEL-6/compose/SAM/x86_64/os/"
+    default_sam_build = "latest-SAM-1.4-RHEL-6"
     def install_host(self):
         host_ip = "10.66.144.13"
         host_user = "root"
-        host_passwd = "autosam"
+        host_passwd = "gaoshang"
         return host_ip, host_user, host_passwd
 
     def install_guest(self, host_ip, host_user, host_passwd, guest_name, guest_compose=default_rhel_build):
@@ -18,8 +19,8 @@ class SAM_LOCAL_INSTALL(SAM_Install_Base):
         guest_ip, guest_user, guest_passwd = virsh_command.create_vm(guest_name, guest_compose)
         return guest_ip, guest_user, guest_passwd
 
-    def install_product(self):
-        self.install_sam(self.default_sam_build)
+    def install_product(self, guest_ip, guest_user, guest_passwd, compose):
+        self.install_sam(compose, guest_ip, guest_user, guest_passwd)
 
     def clone_sam_guest(self, host_ip, host_user, host_passwd, guest_name):
         virsh_command = VirshCommand(host_ip, host_user, host_passwd)
@@ -36,5 +37,17 @@ class SAM_LOCAL_INSTALL(SAM_Install_Base):
         self.install_product(guest_ip, guest_user, guest_passwd, sam_compose)
         self.clone_sam_guest(host_ip, host_user, host_passwd, sam_compose)
 
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            self.start(self.default_sam_build)
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
+
 if __name__ == "__main__":
-    SAM_LOCAL_INSTALL().start("SAM-1.4.1-RHEL-6-20141009.0") 
+    unittest.main()
