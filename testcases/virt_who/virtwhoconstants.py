973c162481cbf169f224da75eb046fa0e852cb57
sgao <sgao@redhat.com>
2015-06-12 10:15:10 +0800

    change esx guest to 7.1

--- testcases/virt_who/virtwhoconstants.py
+++ testcases/virt_who/virtwhoconstants.py
@@ -17,7 +17,7 @@ class VIRTWHOConstants(object):
                     "SAM_PASS":"admin",
 
                     "ESX_HOST":"10.66.128.77",
-                    "ESX_GUEST_NAME":"ESX_6.5_Server_x86_64",
+                    "ESX_GUEST_NAME":"ESX_7.1_Server_x86_64",
 
                     "VIRTWHO_ESX_OWNER" : "ACME_Corporation",
                     "VIRTWHO_ESX_ENV" : "Library",
