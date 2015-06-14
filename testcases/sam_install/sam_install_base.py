8ded18c0a2ef42a26e85e6c4a3322ead360bded2
root <root@dhcp-128-75.nay.redhat.com>
2015-06-14 23:44:55 +0800

    try to resolve katello install time out

--- testcases/sam_install/sam_install_base.py
+++ testcases/sam_install/sam_install_base.py
@@ -108,7 +108,7 @@ class SAM_Install_Base(unittest.TestCase):
     def __install_katello(self, server_ip=None, server_user=None, server_passwd=None):
         cmd = "yum install -y katello-headpin-all"
         # cmd = "yum install -y git"
-        ret, output = self.runcmd(cmd, "yum install -y katello-headpin-all", server_ip, server_user, server_passwd, timeout=3600)
+        ret, output = self.runcmd(cmd, "yum install -y katello-headpin-all && echo done", server_ip, server_user, server_passwd, timeout=3600)
         # here it always time out, need to research reason
 #         if ret == 0:
 #             logger.info("Succeeded to run yum install -y katello-headpin-all.")
