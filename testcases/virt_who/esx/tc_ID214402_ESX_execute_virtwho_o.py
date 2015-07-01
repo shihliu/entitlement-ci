6876c8da9dee16ad6afd75ec21b70e615d52535d
Eko <hongsun924@gmail.com>
2015-07-01 10:56:07 +0800

    fixed tc_ID202507 and tc_ID214402

--- testcases/virt_who/esx/tc_ID214402_ESX_execute_virtwho_o.py
+++ testcases/virt_who/esx/tc_ID214402_ESX_execute_virtwho_o.py
@@ -13,15 +13,21 @@ class tc_ID214402_ESX_execute_virtwho_o(VIRTWHOBase):
             # (2)Execute virt-who in the one-shot mode.
             cmd = "virt-who -o -d"
             ret, output = self.runcmd(cmd, "executing virt-who with one-shot mode")
-            if ret == 0 :
-                # check the status of virt-who
-                cmd = "ps -ef | grep virt-who"
-                ret, output = self.runcmd(cmd, "check the process of virt-who with one-shot mode")
-                if ret == 0 and "DEBUG" in output and "ERROR" not in output:
-                    logger.info("Succeeded to execute virt-who with one-shot mode.")
-                    self.assert_(True, case_name)
-                else:
-                    raise FailException("Failed to execute virt-who with one-shot mode.")
+            if ret == 0 and "DEBUG" in output and "ERROR" not in output:
+                logger.info("Succeeded to execute virt-who with one-shot mode.")
+            else:
+                raise FailException("Failed to execute virt-who with one-shot mode.")
+
+            # (3)Check the status of virt-who
+            # if 'virt-who -o -d' finished, 'ps -ef' shoud can't show any virt-who process because the one-shot mode! 
+            cmd = "ps -ef | grep -E 'virtwho|virt-who'"
+            ret, output = self.runcmd(cmd, "check the process of virt-who with one-shot mode")
+            if ret == 0  and "virtwho.py -o -d" not in output:
+                logger.info("All the virt-who processes exit successfully!")
+                self.assert_(True, case_name)
+            else:
+                raise FailException("Failed to stop virt-who process.")
+
         except Exception, e:
             logger.error("Test Failed - ERROR Message:" + str(e))
             self.assert_(False, case_name)
