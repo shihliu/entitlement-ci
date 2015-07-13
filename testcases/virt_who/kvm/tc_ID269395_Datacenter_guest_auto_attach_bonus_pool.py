7685a2d7ac33932849b57a0d6278e23682d7e1a3
shihliu <shihliu@redhat.com>
2015-07-13 16:47:12 +0800

    update iptables and add three cased

---
+++ testcases/virt_who/kvm/tc_ID269395_Datacenter_guest_auto_attach_bonus_pool.py
@@ -0,0 +1,57 @@
+from utils import *
+from testcases.virt_who.virtwhobase import VIRTWHOBase
+from testcases.virt_who.virtwhoconstants import VIRTWHOConstants
+from utils.exception.failexception import FailException
+
+class tc_ID269395_Datacenter_guest_auto_attach_bonus_pool(VIRTWHOBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            SAM_IP = get_exported_param("SAM_IP")
+            SAM_HOSTNAME = get_exported_param("SAM_HOSTNAME")
+            SAM_USER = VIRTWHOConstants().get_constant("SAM_USER")
+            SAM_PASS = VIRTWHOConstants().get_constant("SAM_PASS")
+
+            guest_name = VIRTWHOConstants().get_constant("KVM_GUEST_NAME")
+
+            test_sku = VIRTWHOConstants().get_constant("datacenter_sku_id")
+            guest_bonus_sku = VIRTWHOConstants().get_constant("datacenter_bonus_sku_id")
+            bonus_quantity = VIRTWHOConstants().get_constant("datacenter_bonus_quantity")
+            sku_name = VIRTWHOConstants().get_constant("datacenter_name")
+
+            self.vw_start_guests(guest_name)
+            guestip = self.kvm_get_guest_ip(guest_name)
+
+            # register guest to SAM
+            if not self.sub_isregistered(guestip):
+                self.configure_host(SAM_HOSTNAME, SAM_IP, guestip)
+                self.sub_register(SAM_USER, SAM_PASS, guestip)
+            # subscribe the host to the physical pool which can generate bonus pool
+            self.sub_subscribe_sku(test_sku)
+            # guest auto subscribe bonus pool
+            cmd = "subscription-manager subscribe --auto"
+            ret, output = self.runcmd(cmd, "guest auto subscribe bonus pool", guestip)
+            if ret == 0:
+                if "Successfully " in output:
+                    logger.info("Succeeded to auto subscribe bonus pool on %s." % self.get_hg_info(guestip))
+                else:
+                    raise FailException("Failed to auto subscribe bonus pool on  %s." % self.get_hg_info(guestip))
+            else:
+                raise FailException("Failed to auto subscribe to a pool on %s." % self.get_hg_info(guestip))
+            # list consumed subscriptions on guest
+            self.check_consumed_status(self, guest_bonus_sku, "SubscriptionName", sku_name, guestip)
+
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            self.sub_unregister(guestip)
+            # unsubscribe host
+            self.sub_unsubscribe()
+            self.vw_stop_guests(guest_name)
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
+
+if __name__ == "__main__":
+    unittest.main()
