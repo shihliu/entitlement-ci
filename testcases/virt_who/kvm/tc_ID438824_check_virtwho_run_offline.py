4ec206c06634cf0ced95a6a3309a11bea531aef0
shihliu <root@localhost.localdomain>
2015-08-11 11:29:35 +0800

    update limited sku

---
+++ testcases/virt_who/kvm/tc_ID438824_check_virtwho_run_offline.py
@@ -0,0 +1,22 @@
+from utils import *
+from testcases.virt_who.virtwhobase import VIRTWHOBase
+from testcases.virt_who.virtwhoconstants import VIRTWHOConstants
+from utils.exception.failexception import FailException
+import pexpect
+
+class tc_ID438824_check_virtwho_run_offline(VIRTWHOBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            self.virt_who_generate_key()
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
+
+
+if __name__ == "__main__":
+    unittest.main()
