7031168df471e43b2b7344682df325434805a624
sgao <sgao@redhat.com>
2015-08-21 10:36:18 +0800

    debug rhsm, skip case for satellite

--- testcases/rhsm/rhsm_level_1/tc_ID143330_configure_to_remove_redhatrepo.py
+++ testcases/rhsm/rhsm_level_1/tc_ID143330_configure_to_remove_redhatrepo.py
@@ -8,40 +8,44 @@ class tc_ID143330_configure_to_remove_redhatrepo(RHSMBase):
     def test_run(self):
         case_name = self.__class__.__name__
         logger.info("========== Begin of Running Test Case %s ==========" % case_name)
-
         try:
-            # [A] - prepare test env
-            # register to server
-            username = RHSMConstants().get_constant("username")
-            password = RHSMConstants().get_constant("password")
-            self.sub_register(username, password)
-            # auto subscribe to a pool
-            autosubprod = RHSMConstants().get_constant("autosubprod")
-            self.sub_autosubscribe(autosubprod)
-            # [B] - run the test
-            # list available repos
-            cmd = "subscription-manager repos --list"
-            (ret, output) = self.runcmd(cmd, "list available repos")
-            productrepo = RHSMConstants().get_constant("productrepo")
-            betarepo = RHSMConstants().get_constant("betarepo")
-            if ret == 0 and productrepo in output and betarepo in output:
-                logger.info("It's successful to list available repos.")
+            test_server = get_exported_param("SERVER_TYPE")
+            if test_server == "SATELLITE" :
+                logger.info("satellite do not support, this test case is skipped ...")
+                self.assert_(True, case_name)
             else:
-                raise FailException("Test Failed - Failed to list available repos.")
-            # check redhat.repo existed in repo dir
-            self.is_redhatrepo_exist(True)
-            # configure to remove redhat.repo
-            self.configure_to_handle_redhatrepo("off")
-           # do a list to make disable work at once
-            cmd = "subscription-manager repos --list"
-            (ret, output) = self.runcmd(cmd, "list available repos")
-            if ret == 0 and "Repositories disabled by configuration." in output:
-                logger.info("It's successful to verify no repo listed after configure to remove redhat.repo")
-            else:
-                raise FailException("Test Failed - Failed to disable available repos")
-            # check redhat.repo file be deleted
-            self.is_redhatrepo_exist(False)
-            self.assert_(True, case_name)
+                # [A] - prepare test env
+                # register to server
+                username = RHSMConstants().get_constant("username")
+                password = RHSMConstants().get_constant("password")
+                self.sub_register(username, password)
+                # auto subscribe to a pool
+                autosubprod = RHSMConstants().get_constant("autosubprod")
+                self.sub_autosubscribe(autosubprod)
+                # [B] - run the test
+                # list available repos
+                cmd = "subscription-manager repos --list"
+                (ret, output) = self.runcmd(cmd, "list available repos")
+                productrepo = RHSMConstants().get_constant("productrepo")
+                betarepo = RHSMConstants().get_constant("betarepo")
+                if ret == 0 and productrepo in output and betarepo in output:
+                    logger.info("It's successful to list available repos.")
+                else:
+                    raise FailException("Test Failed - Failed to list available repos.")
+                # check redhat.repo existed in repo dir
+                self.is_redhatrepo_exist(True)
+                # configure to remove redhat.repo
+                self.configure_to_handle_redhatrepo("off")
+                # do a list to make disable work at once
+                cmd = "subscription-manager repos --list"
+                (ret, output) = self.runcmd(cmd, "list available repos")
+                if ret == 0 and "Repositories disabled by configuration." in output:
+                    logger.info("It's successful to verify no repo listed after configure to remove redhat.repo")
+                else:
+                    raise FailException("Test Failed - Failed to disable available repos")
+                # check redhat.repo file be deleted
+                self.is_redhatrepo_exist(False)
+                self.assert_(True, case_name)
         except Exception, e:
             logger.error(str(e))
             self.assert_(False, case_name)
