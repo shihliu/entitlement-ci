b35c723fbc0342218d015aa6f83d340b057481f0
shihliu <root@localhost.localdomain>
2015-09-07 18:03:16 +0800

    add rhel+rhevm set up env function

---
+++ testcases/virt_who/virtwho_rhel_rhevm_setup.py
@@ -0,0 +1,24 @@
+from utils import *
+from testcases.virt_who.virtwhobase import VIRTWHOBase
+from testcases.virt_who.virtwhoconstants import VIRTWHOConstants
+from utils.exception.failexception import FailException
+
+class virtwho_rhel_rhevm_setup(VIRTWHOBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            #self.rhel_rhevm_sys_setup()
+            #self.rhel_rhevm_sys_setup(get_exported_param("REMOTE_IP_2"))
+            self.rhel_rhevm_setup()
+            #self.generate_ssh_key()
+            #self.install_desktop()
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
+
+if __name__ == "__main__":
+    unittest.main()
