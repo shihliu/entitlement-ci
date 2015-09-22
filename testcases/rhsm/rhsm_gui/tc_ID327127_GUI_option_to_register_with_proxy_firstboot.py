1230e6e85965465730126e7b00c207f3be16f4e6
root <root@dhcp-128-75.nay.redhat.com>
2015-09-22 16:15:59 +0800

    resolve case run error

--- testcases/rhsm/rhsm_gui/tc_ID327127_GUI_option_to_register_with_proxy_firstboot.py
+++ testcases/rhsm/rhsm_gui/tc_ID327127_GUI_option_to_register_with_proxy_firstboot.py
@@ -33,24 +33,26 @@ class tc_ID327127_GUI_option_to_register_with_proxy_firstboot(RHSMGuiBase):
     def test_run(self):
         case_name = self.__class__.__name__
         logger.info("========== Begin of Running Test Case %s ==========" % self.__class__.__name__)
-        if not self.skip_test_on_rhel7():
+        try:
             try:
-                try:
+                if self.skip_test_on_rhel7():
+                    self.assert_(True, case_name)
+                else:
                     self.restore_firstboot_environment()
                     self.open_firstboot()
                     self.click_firstboot_fwd_button()
                     self.check_object_exist('firstboot-main-window', 'configure-proxy-button')
                     self.close_firstboot()
                     self.assert_(True, case_name)
-                except Exception, e:
-                    logger.error("Test Failed - ERROR Message:" + str(e))
-                    self.assert_(False, case_name)
-            finally:
-                self.capture_image(case_name)
-                # need to restore firstboot environment
-                self.restore_firstboot_environment()
-                self.restore_gui_environment()
-                logger.info("========== End of Running Test Case: %s ==========" % case_name)
+            except Exception, e:
+                logger.error("Test Failed - ERROR Message:" + str(e))
+                self.assert_(False, case_name)
+        finally:
+            self.capture_image(case_name)
+            # need to restore firstboot environment
+            self.restore_firstboot_environment()
+            self.restore_gui_environment()
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
 
 if __name__ == "__main__":
     unittest.main()
