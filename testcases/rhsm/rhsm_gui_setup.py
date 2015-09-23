e0fb2f276b777874dd1654d542ee0696e3b2d6a3
root <root@dhcp-128-75.nay.redhat.com>
2015-09-23 21:12:40 +0800

    change rhsm rhsm_gui setup base class

--- testcases/rhsm/rhsm_gui_setup.py
+++ testcases/rhsm/rhsm_gui_setup.py
@@ -1,16 +1,16 @@
 from utils import *
 from utils.tools.shell import command
-from testcases.base import Base
+from testcases.rhsm.rhsmguibase import RHSMGuiBase
 from utils.exception.failexception import FailException
 from testcases.rhsm.rhsmconstants import RHSMConstants
 
-class rhsm_gui_setup(unittest.TestCase):
+class rhsm_gui_setup(RHSMGuiBase):
     def test_run(self):
         case_name = self.__class__.__name__
         logger.info("========== Begin of Running Test Case %s ==========" % case_name)
         try:
             self.rhsm_gui_sys_setup()
-            Base().configure_testing_server()
+            self.configure_testing_server()
             self.assert_(True, case_name)
         except Exception, e:
             logger.error("Test Failed - ERROR Message:" + str(e))
