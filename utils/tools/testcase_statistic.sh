e9c9b68792ec623d8005bf604cd45bfb9059ef07
sgao <sgao@redhat.com>
2015-09-24 16:30:30 +0800

    adjust case anylise format

--- utils/tools/testcase_statistic.sh
+++ utils/tools/testcase_statistic.sh
@@ -5,14 +5,12 @@ for layer_1 in `ls $case_folder`
 do
     if [ -d $case_folder/$layer_1 ]
     then
-        echo $layer_1
         for layer_2 in `ls $case_folder/$layer_1`
         do
             if [ -d $case_folder/$layer_1/$layer_2 ]
             then
-                echo "  "$layer_2
                 case_num=`ls $case_folder/$layer_1/$layer_2/*.py | grep -v "__init__" | wc -l`
-                echo "    total: $case_num"
+                echo "$layer_2: total $case_num"
             fi
         done
     fi
