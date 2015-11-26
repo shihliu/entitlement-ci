11447aafab0ecb6dd5ad2c5e4b403bce370669b5
shihliu <root@dhcp-128-63.nay.redhat.com>
2015-11-26 17:29:24 +0800

    add 155138

---
+++ testcases/virt_who/rhevm/tc_ID155138_RHEVM_validate_unlimited_bonus_pool_creation.py
@@ -0,0 +1,57 @@
+from utils import *
+from testcases.virt_who.vdsmbase import VDSMBase
+from utils.exception.failexception import FailException
+
+class tc_ID155138_RHEVM_validate_unlimited_bonus_pool_creation(VDSMBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            SERVER_IP, SERVER_HOSTNAME, SERVER_USER, SERVER_PASS = self.get_server_info()
+
+            guest_name = self.get_vw_cons("RHEL_RHEVM_GUEST_NAME")
+            rhevm_ip = get_exported_param("RHEVM_IP")
+
+            test_sku = self.get_vw_cons("productid_unlimited_guest")
+            bonus_quantity = self.get_vw_cons("guestlimit_unlimited_guest")
+            sku_name = self.get_vw_cons("productname_unlimited_guest")
+
+            self.rhevm_start_vm(guest_name, rhevm_ip)
+            (guestip,hostuuid) = self.rhevm_get_guest_ip(guest_name, rhevm_ip)
+
+            # register guest to SAM
+            if not self.sub_isregistered(guestip):
+                self.configure_server(SERVER_IP, SERVER_HOSTNAME, guestip)
+                self.sub_register(SERVER_USER, SERVER_PASS, guestip)
+            # subscribe the hypervsior to the physical pool which can generate bonus pool
+            self.server_subscribe_system(hostuuid, self.get_poolid_by_SKU(test_sku),SERVER_IP)
+
+            # list available pools of guest, check related bonus pool generated.
+            new_available_poollist = self.sub_listavailpools(test_sku, guestip)
+            if new_available_poollist != None:
+                for item in range(0, len(new_available_poollist)):
+                    if "Temporary" in new_available_poollist[item]["SubscriptionType"]:
+                        raise FailException("virt-who failed to get host/guest mapping.")
+                    if "Available" in new_available_poollist[item]:
+                        SKU_Number = "Available"
+                    else:
+                        SKU_Number = "Quantity"
+                    if new_available_poollist[item]["SKU"] == test_sku and self.check_type_virtual(new_available_poollist[item]) and new_available_poollist[item][SKU_Number] == bonus_quantity:
+                        logger.info("Succeeded to list bonus pool of product %s" % sku_name) 
+                self.assert_(True, case_name)
+            else:
+                raise FailException("Failed to get available pool list from guest.")
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            if guestip != None and guestip != "":
+                self.sub_unregister(guestip)
+            # unregister the hypervisor
+            self.server_unsubscribe_all_system(hostuuid, SERVER_IP)
+            self.rhevm_stop_vm(guest_name, rhevm_ip)
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
+
+if __name__ == "__main__":
+    unittest.main()
