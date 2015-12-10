5a8be47f0442ec808d61e69f2cda3ee30a1f0527
shihliu <root@dhcp-128-63.nay.redhat.com>
2015-12-10 16:52:42 +0800

    add hyperv socket client and update 174959

---
+++ testcases/virt_who/virtwho_hyperv_setup.py
@@ -0,0 +1,19 @@
+from utils import *
+from testcases.virt_who.hypervbase import HYPERVBase
+from utils.exception.failexception import FailException
+
+class virtwho_hyperv_setup(HYPERVBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            self.hyperv_run_cmd("gethost")
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
+
+if __name__ == "__main__":
+    unittest.main()
