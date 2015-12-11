f31daec7c038bd1b9ffa00115e72e55f8a400a42
shihliu <root@dhcp-128-63.nay.redhat.com>
2015-12-11 17:40:32 +0800

    update recv data

--- testcases/virt_who/hypervbase.py
+++ testcases/virt_who/hypervbase.py
@@ -6,11 +6,18 @@ import socket
 class HYPERVBase(VIRTWHOBase):
     def hyperv_run_cmd(self, cmd, targetmachine_ip=""):
         hyperv_ip = "10.66.128.9"
+#         hyperv_port = get_exported_param("HYPERV_PORT")
+#         logger.info("hyperv_port is %s" % hyperv_port)        
         hyperv_port = 6555
 #         hyperv_port = int(hyperv_port_old)
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      # listen to socket
-        s.connect((hyperv_ip,hyperv_port))       # connect to the socket
-        s.sendall(cmd)      # send the command
-        data=s.recv(4096)     # get all the output
-        logger.info ("After run %s, the output is \n%s" %(cmd, data))
-        s.close()   # close the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # listen to socket
+        s.connect((hyperv_ip, hyperv_port))  # connect to the socket
+        s.sendall(cmd)  # send the command
+        data = ""
+        while True:
+          buf = s.recv(1024)
+          if not len(buf):
+            break
+          data = data + buf
+        logger.info ("After run %s, the output is \n%s" % (cmd, data))
+        s.close()  # close the socket
