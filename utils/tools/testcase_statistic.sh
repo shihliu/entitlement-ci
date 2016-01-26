5c98675d9b97dfd2db508aa43431e006e450f59f
bluesky-sgao <sgao@redhat.com>
2016-01-26 13:31:44 +0800

    run virt-who under virt_who_polarion

--- utils/tools/testcase_statistic.sh
+++ utils/tools/testcase_statistic.sh
@@ -5,13 +5,16 @@ for layer_1 in `ls $case_folder`
 do
     if [ -d $case_folder/$layer_1 ]
     then
-        for layer_2 in `ls $case_folder/$layer_1`
-        do
-            if [ -d $case_folder/$layer_1/$layer_2 ]
-            then
-                case_num=`ls $case_folder/$layer_1/$layer_2/*.py | grep -v "__init__" | wc -l`
-                echo "$layer_2: total $case_num"
-            fi
-        done
+        if [ $layer_1 != virt_who ]
+        then
+            for layer_2 in `ls $case_folder/$layer_1`
+            do
+                if [ -d $case_folder/$layer_1/$layer_2 ]
+                then
+                    case_num=`ls $case_folder/$layer_1/$layer_2/*.py | grep -v "__init__" | wc -l`
+                    echo "$layer_2: total $case_num"
+                fi
+            done
+        fi
     fi
 done
