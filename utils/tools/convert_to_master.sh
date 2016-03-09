809b4446d1065ae8425a502c5dce6f2027bac577
bluesky-sgao <sgao@redhat.com>
2016-03-09 18:59:17 +0800

    convert jobs from unified master to provate

--- utils/tools/convert_to_master.sh
+++ utils/tools/convert_to_master.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ls *.yaml | grep -v "defaults\|triggers" | xargs -i -d "\n" sed -i -e "s/node: master/node: '{jmastername}'/g" -e "/    jslavename:/a\    jmastername: jslave-static-entitlement-master" {}
+# if no parameter provided, convert to unified master jobs, else convert back
+if [ ! -n "$1" ]
+then
+    echo "convert to unified master jobs"
+    ls *.yaml | grep -v "defaults\|triggers" | xargs -i -d "\n" sed -i -e "s/node: master/node: '{jmastername}'/g" -e "/    jslavename:/a\    jmastername: jslave-static-entitlement-master" {}
+else
+    echo "convert to private master jobs"
+    ls *.yaml | grep -v "defaults\|triggers" | xargs -i -d "\n" sed -i -e "s/node: '{jmastername}'/node: master/g" -e "/    jmastername: jslave-static-entitlement-master/d" {}
+fi
