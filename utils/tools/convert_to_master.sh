1e4d6ecb4370c1c0e3c6c942777599e2b0e946f3
bluesky-sgao <sgao@redhat.com>
2016-03-14 15:10:21 +0800

    convert jobs with trigger job also

--- utils/tools/convert_to_master.sh
+++ utils/tools/convert_to_master.sh
@@ -3,8 +3,8 @@
 if [ ! -n "$1" ]
 then
     echo "convert to unified master jobs"
-    ls *.yaml | grep -v "defaults\|triggers" | xargs -i -d "\n" sed -i -e "s/node: master/node: '{jmastername}'/g" -e "/    jslavename:/a\    jmastername: jslave-static-entitlement-master" {}
+    ls *.yaml | xargs -i -d "\n" sed -i -e "s/node: master/node: '{jmastername}'/g" -e "/    jslavename:/a\    jmastername: jslave-static-entitlement-master" {}
 else
     echo "convert to private master jobs"
-    ls *.yaml | grep -v "defaults\|triggers" | xargs -i -d "\n" sed -i -e "s/node: '{jmastername}'/node: master/g" -e "/    jmastername: jslave-static-entitlement-master/d" {}
+    ls *.yaml | xargs -i -d "\n" sed -i -e "s/node: '{jmastername}'/node: master/g" -e "/    jmastername: jslave-static-entitlement-master/d" {}
 fi
