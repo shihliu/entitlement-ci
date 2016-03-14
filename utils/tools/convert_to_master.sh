2f32c8ba5af51ce5d448ac97755772a100591bf8
bluesky-sgao <sgao@redhat.com>
2016-03-14 15:53:53 +0800

    modify convert file

--- utils/tools/convert_to_master.sh
+++ utils/tools/convert_to_master.sh
@@ -3,7 +3,8 @@
 if [ ! -n "$1" ]
 then
     echo "convert to unified master jobs"
-    ls *.yaml | xargs -i -d "\n" sed -i -e "s/node: master/node: '{jmastername}'/g" -e "/    jslavename:/a\    jmastername: jslave-static-entitlement-master" {}
+    ls *.yaml | xargs -i -d "\n" sed -i -e "s/node: master/node: '{jmastername}'/g" {}
+    ls *.yaml | grep -v "virt-who-defaults.yaml" | xargs -i -d "\n" sed -i -e "/    jmastername: jslave-static-entitlement-master/d" -e "\$a\    jmastername: jslave-static-entitlement-master" {}
 else
     echo "convert to private master jobs"
     ls *.yaml | xargs -i -d "\n" sed -i -e "s/node: '{jmastername}'/node: master/g" -e "/    jmastername: jslave-static-entitlement-master/d" {}
