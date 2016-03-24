76bee9a400118d6ec8e2b44bb7fc27895c814954
bluesky-sgao <sgao@redhat.com>
2016-03-24 15:13:24 +0800

    rpm query with rpm -q *

--- testcases/base.py
+++ testcases/base.py
@@ -74,7 +74,7 @@ class Base(unittest.TestCase):
             raise FailException("Test Failed - Failed to check file: %s mode as: %s" % (file, mode))
 
     def cm_get_rpm_version(self, rpm, targetmachine_ip=""):
-        ret, output = self.runcmd("rpm -qa | grep %s" % rpm, "get %s version" % rpm)
+        ret, output = self.runcmd("rpm -q %s" % rpm, "get %s version" % rpm)
         version = output.strip()
         if ret == 0:
             logger.info("Succeeded to get %s version %s." % (rpm, version))
