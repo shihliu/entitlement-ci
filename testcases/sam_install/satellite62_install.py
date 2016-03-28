89ca4b0260328793f6a00f6e11fbc5cb194b9057
bluesky-sgao <sgao@redhat.com>
2016-03-28 13:59:24 +0800

    install rhevm 36 for virt-who testing

--- testcases/sam_install/satellite62_install.py
+++ testcases/sam_install/satellite62_install.py
@@ -1,7 +1,7 @@
 from utils import *
 from testcases.sam_install.sam_install_base import SAM_Install_Base
 
-class SATELLITE_Install(SAM_Install_Base):
+class SATELLITE62_Install(SAM_Install_Base):
     def test_run(self):
         case_name = self.__class__.__name__
         logger.info("========== Begin of Running Test Case %s ==========" % case_name)
