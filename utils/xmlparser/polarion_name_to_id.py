4cdeb682621b93231b22d50cce2ef768e0e5814e
bluesky-sgao <sgao@redhat.com>
2016-04-28 14:30:48 +0800

    debug

--- utils/xmlparser/polarion_name_to_id.py
+++ utils/xmlparser/polarion_name_to_id.py
@@ -3,7 +3,7 @@ from utils.xmlparser.xmlparser import XMLParser
 
 class Polarion_Name_To_ID(XMLParser):
 
-    def name_to_id(self):
+    def name_to_id(self, polarion_txt):
         testcases = self.root.getElementsByTagName("testcase")
         if testcases.length == 0:
             logger.info("No test case found in %s" % self.xml_file)
@@ -14,8 +14,9 @@ class Polarion_Name_To_ID(XMLParser):
                 case_id = re.findall(r"(?<=_ID).*?(?=_)", classname, re.I)[0]
                 polarion_case_id = "RHEL6-" + case_id
                 testcase.setAttribute("name", polarion_case_id)
+                self.write_file(polarion_txt, polarion_case_id + "=" + polarion_case_id)
                 self.write_xml()
-            logger.info("Succeeded to convert name to id in %s" % self.xml_file)
+            logger.info("Succeeded to convert name to id in %s and add name:id match info to %s" % (self.xml_file, polarion_txt))
 
     def write_file(self, mfile, content):
         fileHandle = open(mfile, 'a')
@@ -24,5 +25,6 @@ class Polarion_Name_To_ID(XMLParser):
 
 if __name__ == "__main__":
     builds_xml = sys.argv[1]
+    polarion_txt = sys.argv[2]
     polarion_name_to_id = Polarion_Name_To_ID(builds_xml)
-    polarion_name_to_id.name_to_id()
+    polarion_name_to_id.name_to_id(polarion_txt)
