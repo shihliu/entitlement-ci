a25a980acb9d63d045dfc75ccc7e1bea970cb7b5
bluesky-sgao <sgao@redhat.com>
2016-04-28 15:56:54 +0800

    change guest back

--- testcases/virt_who_polarion/virtwhoconstants.py
+++ testcases/virt_who_polarion/virtwhoconstants.py
@@ -8,7 +8,7 @@ class VIRTWHOConstants(object):
     "beaker_image_server":              "10.16.96.131:/home/samdata",
 
     # for kvm
-    "KVM_GUEST_NAME":                   "7.1_Server_x86_64",
+    "KVM_GUEST_NAME":                   "6.7_Server_x86_64",
     "nfs_image_path":                   "/root/images_nfs",
     "local_mount_point":                "/tmp/images_mnt",
 
