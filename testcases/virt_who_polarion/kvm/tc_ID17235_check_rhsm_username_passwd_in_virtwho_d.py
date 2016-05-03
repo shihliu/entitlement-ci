094e6cc47a28a7b17b19f763d533157710678454
shihliu <root@dhcp-128-63.nay.redhat.com>
2016-05-03 11:28:29 +0800

    add kvm 17219 17220 17224 17225 17235

---
+++ testcases/virt_who_polarion/kvm/tc_ID17235_check_rhsm_username_passwd_in_virtwho_d.py
@@ -0,0 +1,33 @@
+from utils import *
+from testcases.virt_who_polarion.kvmbase import KVMBase
+from utils.exception.failexception import FailException
+
+class tc_ID17235_check_rhsm_username_passwd_in_virtwho_d(KVMBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            remote_ip_2 = get_exported_param("REMOTE_IP_2")
+            server_ip, server_hostname, server_user, server_pass = self.get_server_info()
+            self.update_config_to_default(remote_ip_2)
+            self.update_vw_configure(targetmachine_ip=remote_ip_2)
+            self.runcmd_service("stop_virtwho", remote_ip_2)
+
+            # (1) Config libvirt mode in /etc/virt-who.d with correct rhsm_username and rhsm_password
+            self.set_rhsm_user_pass("libvirt", server_user, server_pass, remote_ip_2)
+            self.vw_check_mapping_info_number_in_rhsm_log(targetmachine_ip=remote_ip_2)
+            # (2) Config libvirt mode in /etc/virt-who.d with wrong rhsm_username and rhsm_password
+            self.set_rhsm_user_pass("libvirt", server_user, "xxxxxxxx", remote_ip_2)
+            self.vw_check_message_in_rhsm_log("BUG yet", targetmachine_ip=remote_ip_2)
+
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            self.unset_all_virtwho_d_conf(remote_ip_2)
+            self.runcmd_service("restart_virtwho", remote_ip_2)
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
+
+if __name__ == "__main__":
+    unittest.main()
