29b63b082901524601d8eed699cd5b60e55994ec
bluesky-sgao <sgao@redhat.com>
2016-05-16 20:06:48 +0800

    fix convert job to unified master bug

--- utils/tools/convert_to_master.sh
+++ utils/tools/convert_to_master.sh
@@ -4,7 +4,8 @@ if [ ! -n "$1" ]
 then
     echo "convert to unified master jobs"
     ls *.yaml | xargs -i -d "\n" sed -i -e "s/node: master/node: '{jmastername}'/g" {}
-    ls *.yaml | grep -v "virt-who-defaults.yaml" | xargs -i -d "\n" sed -i -e "/    jmastername: jslave-static-entitlement-master/d" -e "\$a\    jmastername: jslave-static-entitlement-master" {}
+    ls *.yaml | grep -v "virt-who-defaults.yaml" | xargs -i -d "\n" sed -i -e "/    jmastername: jslave-static-entitlement-master/d" {}
+    ls *.yaml | grep -v "virt-who-defaults.yaml" | xargs -i -d "\n" sed -i -e "\$a\    jmastername: jslave-static-entitlement-master" {}
     ls *.yaml | xargs -i -d "\n" sed -i -e "s/provision_jslave.sh --site/provision_jslave.sh --jenkins_master_username=sgao --jenkins_master_password=0bdd1e1626c4701fc0023fa87474459d --site/g" {}
 else
     echo "convert to private master jobs"
