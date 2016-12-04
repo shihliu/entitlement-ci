f8b31c813952002fd6c959f69f516f1671b7fbb6
shihliu@redhat.com <root@dhcp-128-63.nay.redhat.com>
2016-12-04 15:02:39 +0800

    update

--- provision/satellite_provision.sh
+++ provision/satellite_provision.sh
@@ -43,8 +43,10 @@ docker exec -i $CONTAINER_NAME /usr/sbin/sshd -D &
 # docker exec -i $CONTAINER_NAME yum install -y satellite
 SATELLITE_IP=`docker exec -i $CONTAINER_NAME /sbin/ifconfig eth1 | grep "inet addr:"| awk '{print $2}' | cut -c 6-`
 #SATELLITE_IP=docker exec -i $CONTAINER_NAME /sbin/ifconfig eth1 | grep "inet addr:" | awk '{print $2}' | cut -c 6-
-echo SATELLITE_IP=$SATELLITE_IP>>RESOURCE.txt
-echo SATELLITE_HOSTNAME=$CONTAINER_NAME>>RESOURCE.txt
+echo SATELLITE_IP=$SATELLITE_IP>>RESOURCES.txt
+echo SATELLITE_HOSTNAME=$CONTAINER_NAME>>RESOURCES.txt
+echo export REMOTE_IP=$SATELLITE_IP>>RESOURCES.sh
+echo export SATELLITE_HOSTNAME=$CONTAINER_NAME>>RESOURCES.sh
 export SATELLITE_IP=$SATELLITE_IP
 export SATELLITE_HOSTNAME=$CONTAINER_NAME
 # fi
