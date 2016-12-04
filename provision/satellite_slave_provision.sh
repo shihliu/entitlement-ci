36ce4fc135fea90342aa7a84d9b28c3d676bd5b1
shihliu@redhat.com <root@dhcp-128-63.nay.redhat.com>
2016-12-04 14:06:26 +0800

    add provision/

---
+++ provision/satellite_slave_provision.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Parse command line arguments
+for i in "$@"
+do
+case $i in
+    --site=*)
+    SITE=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    --ssh_keyfile=*)
+    SSH_KEYFILE=`echo $i | sed 's/[-a-zA-Z0-9_]*=//'`
+    ;;
+    --imagename=*)
+    IMAGE_NAME=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    --containername=*)
+    CONTAINER_NAME=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    --help)
+    HELP=True
+    ;;
+    *)
+    echo "Unknown option $i"        # unknown option
+    HELP=True
+    ;;
+esac
+done
+
+if [ "$SITE" == "" ]; then SITE="10.16.46.37"; fi
+if [ "$IMAGE_NAME" == "" ]; then IMAGE_NAME="satellite62"; fi
+if [ "$CONTAINER_NAME" == "" ]; then CONTAINER_NAME="satellite62.redhat.com";fi
+# Set some defaults if values not assigned
+# if [ "$DOCKERFILE" == "" ]; then DOCKERFILE="/root/.";
+# else
+  #Build satellite62 IMAGE
+#Provision satellite
+docker run --privileged -itd  --name $CONTAINER_NAME --net=none $IMAGE_NAME bash
+pipework br0  $CONTAINER_NAME  dhclient
+docker exec -i $CONTAINER_NAME hostname $CONTAINER_NAME
+docker exec -i $CONTAINER_NAME /usr/sbin/sshd -D &
+# docker exec -i $CONTAINER_NAME export PATH=$PATH:/sbin/|service sshd restart
+# docker exec -i $CONTAINER_NAME export PATH=$PATH:/sbin/|service sshd status
+# docker exec -i $CONTAINER_NAME yum install -y satellite
+SATELLITE_IP=`docker exec -i $CONTAINER_NAME /sbin/ifconfig eth1 | grep "inet addr:"| awk '{print $2}' | cut -c 6-`
+#SATELLITE_IP=docker exec -i $CONTAINER_NAME /sbin/ifconfig eth1 | grep "inet addr:" | awk '{print $2}' | cut -c 6-
+echo SATELLIE_SLAVE_IP=$SATELLITE_IP>>RESOURCE.txt
+echo SATELLIE_SLAVE_NAME=$CONTAINER_NAME>>RESOURCE.txt
+# fi
+#
+echo "Provisioning with the following environment"
+echo "-------------------------------------------"
+echo "SITE:                     $SITE"
+echo "SATELLIE_SLAVE_NAME:      $CONTAINER_NAME"
+echo "SATELLIE_SLAVE_IP:             $SATELLITE_IP"
