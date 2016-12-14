fd6dcba91d04d90d85221f44b7972de9b76840fb
shihliu@redhat.com <root@dhcp-128-63.nay.redhat.com>
2016-12-14 09:53:29 +0800

    update

--- install/install_base.py
+++ install/install_base.py
@@ -73,13 +73,13 @@ class Install_Base(Base):
             raise FailException("Test Failed - Failed to set /etc/sysconfig/selinux.")
 
     def __set_hostname(self, targetmachine_ip=""):
-        SATELLITE_HOSTNAME = get_exported_param("SATELLITE_HOSTNAME")
-        cmd = "hostname %s" %SATELLITE_HOSTNAME
+        SERVER_HOSTNAME = get_exported_param("SERVER_HOSTNAME")
+        cmd = "hostname %s" %SERVER_HOSTNAME
         ret, output = self.runcmd(cmd, "set satellite hostname", targetmachine_ip)
         if ret == 0:
-            logger.info("Succeeded to set satellite hostname to %s." %SATELLITE_HOSTNAME)
+            logger.info("Succeeded to set satellite hostname to %s." %SERVER_HOSTNAME)
         else:
-            raise FailException("Test Failed - Failed to set satellite hostname to %s." %SATELLITE_HOSTNAME)
+            raise FailException("Test Failed - Failed to set satellite hostname to %s." %SERVER_HOSTNAME)
 
     def __set_hosts_file(self, targetmachine_ip=""):
         cmd = "sed -i '/%s/d' /etc/hosts; echo \"%s `hostname`\" >> /etc/hosts" % (targetmachine_ip, targetmachine_ip)
