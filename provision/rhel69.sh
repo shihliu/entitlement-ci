f6c6b97c07b50b5abf458eed5b6a3819dfe7381c
shihliu@redhat.com <root@localhost.localdomain>
2016-12-19 11:24:55 +0800

    update

--- provision/rhel69.sh
+++ provision/rhel69.sh
@@ -34,32 +34,29 @@ if [ $isRhelExist -eq 0 ]
 then
    echo "old" $IMAGE_NAME "is exist"
    docker rmi -f $IMAGE_NAME
-else
-   pushd $WORKSPACE/entitlement-ci/provision
-   export compose_name=RHEL-6.9-20161216.1
-   echo compose_name=$compose_name
-   export redhat_root='/redhat_image/rootfs'
-   echo redhat_root=$redhat_root
-   if [-d "$redhat_root"]; 
-   then
-      rm -rf $redhat_root
-   fi
-   mkdir -p $redhat_root
-   wget http://10.66.144.9/home/shihliu/define.repo
-   mv define.repo /etc/yum.repos.d
-   echo $compose_name
-   sed -i -e 's/'rhelbuild'/'$compose_name'/g' /etc/yum.repos.d/define.repo
-   rpm --root $redhat_root --initdb
-   yum -y reinstall --downloadonly --downloaddir . redhat-release
-   rpm --root $redhat_root -ivh redhat-release*.rpm
-   rpm --root $redhat_root --import  $redhat_root/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
-   cp /etc/yum.repos.d/define.repo $redhat_root/etc/yum.repos.d
-   yum -y --installroot=$redhat_root --setopt=tsflags='nodocs' --setopt=override_install_langs=en_US.UTF-8 install yum
-   sed -i "/distroverpkg=redhat-release/a override_install_langs=en_US.UTF-8\ntsflags=nodocs" $redhat_root/etc/yum.conf
-   cp /etc/resolv.conf $redhat_root/etc
-   #chroot $redhat_root /bin/bash yum install -y virt-who libvirt
-   chroot $redhat_root /bin/bash yum install -y openssh-server net-tools
-
-   chroot $redhat_root /bin/bash yum clean all
-   tar -C $redhat_root -c . | docker import - $IMAGE_NAME
 fi
+export compose_name=RHEL-6.9-20161216.1
+echo compose_name=$compose_name
+export redhat_root='/redhat_image/rootfs'
+echo redhat_root=$redhat_root
+if [-d "$redhat_root"]; 
+then
+   rm -rf $redhat_root
+fi
+mkdir -p $redhat_root
+wget http://10.66.144.9/home/shihliu/define.repo
+mv define.repo /etc/yum.repos.d
+echo $compose_name
+sed -i -e 's/'rhelbuild'/'$compose_name'/g' /etc/yum.repos.d/define.repo
+rpm --root $redhat_root --initdb
+yum -y reinstall --downloadonly --downloaddir . redhat-release
+rpm --root $redhat_root -ivh redhat-release*.rpm
+rpm --root $redhat_root --import  $redhat_root/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+cp /etc/yum.repos.d/define.repo $redhat_root/etc/yum.repos.d
+yum -y --installroot=$redhat_root --setopt=tsflags='nodocs' --setopt=override_install_langs=en_US.UTF-8 install yum
+sed -i "/distroverpkg=redhat-release/a override_install_langs=en_US.UTF-8\ntsflags=nodocs" $redhat_root/etc/yum.conf
+cp /etc/resolv.conf $redhat_root/etc
+#chroot $redhat_root /bin/bash yum install -y virt-who libvirt
+chroot $redhat_root /bin/bash yum install -y openssh-server net-tools
+chroot $redhat_root /bin/bash yum clean all
+tar -C $redhat_root -c . | docker import - $IMAGE_NAME
