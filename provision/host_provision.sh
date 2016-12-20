8ca149fb645375277ab69bfb08a547cd9bf7b3da
shihliu@redhat.com <root@localhost.localdomain>
2016-12-20 15:16:43 +0800

    update

--- provision/host_provision.sh
+++ provision/host_provision.sh
@@ -30,15 +30,17 @@ if [ "$SITE" == "" ]; then SITE="10.16.46.37"; fi
 if [ "$IMAGE_NAME" == "" ]; then IMAGE_NAME="redhat69"; fi
 if [ "$CONTAINER_NAME" == "" ]; then CONTAINER_NAME="rhel69.redhat.com";fi
 
+export PASS='red2015'
 
 docker run --privileged -itd  --name $CONTAINER_NAME --net=none $IMAGE_NAME bash
 pipework br0  $CONTAINER_NAME  dhclient
 docker exec -i $CONTAINER_NAME hostname $CONTAINER_NAME
-#docker exec -i $CONTAINER_NAME yum install -y openssh-server net-tools passwd
 #docker exec -i $CONTAINER_NAME echo "root:red2015" | chpasswd
-#docker exec -i $CONTAINER_NAME sed -i 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
-#docker exec -i $CONTAINER_NAME ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
-#docker exec -i $CONTAINER_NAME ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
+echo -e "$PASS\n$PASS" | sudo docker exec -i $CONTAINER_NAME passwd
+docker exec -i $CONTAINER_NAME yum install -y openssh-server net-tools passwd
+docker exec -i $CONTAINER_NAME sed -i 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+docker exec -i $CONTAINER_NAME ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
+docker exec -i $CONTAINER_NAME ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
 docker exec -i $CONTAINER_NAME /usr/sbin/sshd -D &
 REMOTE_IP=`docker exec -i $CONTAINER_NAME /sbin/ifconfig eth1 | grep "inet addr:"| awk '{print $2}' | cut -c 6-`
 echo REMOTE_IP=$REMOTE_IP>>RESOURCES.txt
