5fdeba53edc2ee65f6859b780acf9f348e06c7a3
shihliu@redhat.com <root@dhcp-128-63.nay.redhat.com>
2017-02-15 10:13:22 +0800

    update site

--- provision/satellite_provision.sh
+++ provision/satellite_provision.sh
@@ -26,7 +26,7 @@ case $i in
 esac
 done
 
-if [ "$SITE" == "" ]; then SITE="10.66.128.66"; fi
+if [ "$SITE" == "" ]; then SITE="10.66.128.134"; fi
 if [ "$IMAGE_NAME" == "" ]; then IMAGE_NAME="satellite62"; fi
 if [ "$CONTAINER_NAME" == "" ]; then CONTAINER_NAME="satellite62.redhat.com";fi
 
