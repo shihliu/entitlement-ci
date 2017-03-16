7cd09d663865d2e0bfc3f0ba2e04c416cba94c1a
root <root@dhcp-129-210.nay.redhat.com>
2017-03-16 11:36:37 +0800

    add satellite esx cases

--- testcases/virt_who_polarion/satellite/esx/tc_ID82526_ESX_check_mapping_in_sec_config.py
+++ testcases/virt_who_polarion/satellite/esx/tc_ID82526_ESX_check_mapping_in_sec_config.py
@@ -2,21 +2,26 @@ from utils import *
 from testcases.virt_who_polarion.esxbase import ESXBase
 from utils.exception.failexception import FailException
 
-class tc_ID17200_ESX_check_debug_function_by_cli(ESXBase):
+class tc_ID82526_HYPERV_check_mapping_in_sec_config(ESXBase):
     def test_run(self):
         case_name = self.__class__.__name__
         logger.info("========== Begin of Running Test Case %s ==========" % case_name)
         try:
             self.runcmd_service("stop_virtwho")
-            cmd = self.virtwho_cli("esx") + " -d"
-            self.vw_check_message_in_debug_cmd(cmd, "DEBUG")
-            cmd = self.virtwho_cli("esx")
-            self.vw_check_message_in_debug_cmd(cmd, "DEBUG", message_exists=False)
+
+            # (1) Disable esx mode in /etc/sysconfig/virt-who
+            self.config_option_disable("VIRTWHO_HYPERV")
+            # (2) Config esx mode in /etc/virt-who.d
+            self.set_virtwho_sec_config("esx")
+            self.vw_check_mapping_info_number_in_rhsm_log()
+
             self.assert_(True, case_name)
         except Exception, e:
             logger.error("Test Failed - ERROR Message:" + str(e))
             self.assert_(False, case_name)
         finally:
+            self.set_esx_conf()
+            self.unset_all_virtwho_d_conf()
             logger.info("========== End of Running Test Case: %s ==========" % case_name)
 
 if __name__ == "__main__":
