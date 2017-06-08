c62b5ff94d15221643ab23bd5e5e9769b3bcbcc9
root <root@dhcp-129-210.nay.redhat.com>
2017-06-08 08:42:45 +0800

    update generate_ssh_key

--- testcases/virt_who/virtwho_remote_libvirt_setup.py
+++ testcases/virt_who/virtwho_remote_libvirt_setup.py
@@ -1,7 +1,7 @@
 from utils import *
 from testcases.virt_who.virtwhobase import VIRTWHOBase
 
-class virtwho_kvm_setup(VIRTWHOBase):
+class virtwho_remote_libvirt_setup(VIRTWHOBase):
     def test_run(self):
         case_name = self.__class__.__name__
         logger.info("========== Begin of Running Test Case %s ==========" % case_name)
@@ -10,7 +10,7 @@ class virtwho_kvm_setup(VIRTWHOBase):
             self.kvm_static_sys_setup(get_exported_param("REMOTE_IP_1"))
             self.kvm_static_sys_setup(get_exported_param("REMOTE_IP_2"))
             self.remote_libvirt_setup()
-            self.generate_ssh_key_define()
+            self.generate_ssh_key()
 #             self.cm_install_desktop()
             self.assert_(True, case_name)
         except Exception, e:
